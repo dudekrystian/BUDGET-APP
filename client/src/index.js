f83db63190ae280d73c74bbeda3e88ba2d05ba09
dudekrystian <dudekrystian@gmail.com>
2022-11-03 19:09:51 +0100

    wrap the application by context

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BudgetsProvider } from "./contexts/budgetsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
   </React.StrictMode>
 );
