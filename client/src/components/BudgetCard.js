6a019a3744490e735b3309437eef7b40ccde05d4
dudekrystian <dudekrystian@gmail.com>
2022-11-03 18:34:07 +0100

    add ProgressBar to component BudgetCard

---
+++ client/src/components/BudgetCard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import { currencyFormatter } from "../utils/utils";
+
+export default function BudgetCard({ name, amount, max }) {
+  return (
+    <div className="budget-card">
+      <div className="budget-card-amount">
+        <h2>{name}</h2>
+        <span>
+          {currencyFormatter.format(amount)} / {currencyFormatter.format(max)}
+        </span>
+      </div>
+      <ProgressBar
+        bgcolor={getProgressBarColor(amount, max)}
+        completed={getProgressBarColorBarValue(amount, max)}
+      />
+
+      <div className="budget-card-button">
+        <button className="add">Add Expense</button>
+        <button className="list">List Expense</button>
+        <button className="remove-card">Remove All</button>
+      </div>
+    </div>
+  );
+}
+
+function getProgressBarColor(amount, max) {
+  const ratio = (amount / max) * 100;
+
+  console.log(ratio);
+
+  if (ratio <= 50) {
+    return "blue";
+  }
+
+  if (ratio <= 65) {
+    return "green";
+  }
+
+  if (ratio <= 85) {
+    return "orange";
+  }
+
+  if (ratio <= 100) {
+    return "red";
+  }
+}
+
+function getProgressBarColorBarValue(amount, max) {
+  const ratio = (amount / max) * 100;
+
+  return ratio;
+}
