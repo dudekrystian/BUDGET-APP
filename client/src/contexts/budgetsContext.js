c7ecd19e1203a8207c88da628fb3731a7795a49f
dudekrystian <dudekrystian@gmail.com>
2022-11-03 19:08:54 +0100

    create budget context

---
+++ client/src/contexts/budgetsContext.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+
+const BudgetsContext = React.createContext();
+
+export function useBudgets() {
+  return useContext(BudgetsContext);
+}
+
+export const BudgetsProvider = ({ children }) => {
+  return (
+    <BudgetsContext.Provider value={{}}>{children}</BudgetsContext.Provider>
+  );
+};
