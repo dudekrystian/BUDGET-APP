5a786b09878082faba90836c89cb28d1aee2e614
dudekrystian <dudekrystian@gmail.com>
2022-11-13 13:27:28 +0100

    create component to update sum amount with all expense

---
+++ client/src/components/Total.js
@@ -0,0 +1,24 @@
+import React from "react";
+import useBudgets from "../contexts/budgetsContext";
+
+export default function Total() {
+  const { expenses, budgets } = useBudgets();
+
+  const totalAmount = expenses.reduce(
+    (total, expense) => total + expense.amount,
+    0
+  );
+
+  const totalBudgetsAmount = budgets.reduce(
+    (total, bugdet) => total + bugdet.max,
+    0
+  );
+
+  return (
+    <>
+      <label>
+        Total: {totalAmount} / {totalBudgetsAmount}
+      </label>
+    </>
+  );
+}
