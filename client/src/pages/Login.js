9cd98902370e17f1b0a7001a1d16ac6674369706
dudekrystian <dudekrystian@gmail.com>
2022-11-20 12:34:30 +0100

    create login form and useState for mail, password

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login">
+      <div className="login-information">
+        <h1>Welcome to Budget App!</h1>
+        <p>Control your money!</p>
+      </div>
+      <form className="login-form">
+        <h3>Log in app</h3>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email"
+        ></input>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          value={password}
+          placeholder="Password"
+        ></input>
+        <button type="submit">Sing in</button>
+      </form>
+    </div>
+  );
 }
