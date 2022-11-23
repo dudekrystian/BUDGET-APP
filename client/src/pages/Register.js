1d10f30aa13e3f23ca7b6d5ed3054cddbd901002
dudekrystian <dudekrystian@gmail.com>
2022-11-23 14:19:20 +0100

    create register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Register() {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <div className="login-information" onSubmit={handleSubmit}>
+        <h1>Create an accountant</h1>
+        <p>Control your money!</p>
+      </div>
+      <form className="login-form">
+        <h3>Sing Up</h3>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email"
+        ></input>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          value={password}
+          placeholder="Password"
+        ></input>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
