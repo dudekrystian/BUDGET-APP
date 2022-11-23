d01cedf9b1b09b0d0f8d3f8a64437b1eaefe4c0d
dudekrystian <dudekrystian@gmail.com>
2022-11-23 14:31:17 +0100

    fetch data from api to register fuction

--- client/src/hooks/useRegister.js
+++ client/src/hooks/useRegister.js
@@ -1,3 +1,20 @@
 export default function useRegister() {
-  return <div>useRegister</div>;
+  const register = async (email, password) => {
+    const response = await fetch("/api/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      //
+    }
+    if (response.ok) {
+      //
+    }
+  };
+
+  return { register };
 }
