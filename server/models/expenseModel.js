4d635064243f7722b57699bcd6dc228b91d831ee
dudekrystian <dudekrystian@gmail.com>
2022-12-04 19:57:52 +0100

    add to model id_budget

--- server/models/expenseModel.js
+++ server/models/expenseModel.js
@@ -14,6 +14,10 @@ const expenseSchema = new Schema(
       type: Number,
       required: true,
     },
+    budget_id: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
