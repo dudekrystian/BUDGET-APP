3aaabe2d50f95877cb114cb0a3cc37ee10fddc9d
dudekrystian <dudekrystian@gmail.com>
2022-12-04 20:01:50 +0100

    add function fetch api budgets and expenses

--- client/src/contexts/budgetsContext.js
+++ client/src/contexts/budgetsContext.js
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from "react";
-import { v4 as uuidV4 } from "uuid";
+import React, { useContext, useState, useEffect } from "react";
 
 const BudgetsContext = React.createContext();
 
@@ -7,28 +6,55 @@ export default function useBudgets() {
   return useContext(BudgetsContext);
 }
 
-// add fetch api
+//  fetch api
 
 export const BudgetsProvider = ({ children }) => {
   const [budgets, setBudgets] = useState([]);
   const [expenses, setExpenses] = useState([]);
 
+  const fetchBudget = async () => {
+    const response = await fetch("/api/budgets", {
+      method: "GET",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      setBudgets(json);
+    }
+  };
+
+  const fetchExpense = async () => {
+    const response = await fetch("/api/expenses", {
+      method: "GET",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      setExpenses(json);
+    }
+  };
+
+  useEffect(() => {
+    fetchExpense();
+    fetchBudget();
+  }, []);
+
   function getBudgetExpenses(budgetId) {
-    return expenses.filter((expense) => expense.budgetId === budgetId);
+    return expenses.filter((expense) => expense.budget_id === budgetId);
   }
 
-  function addExpense({ description, amount, budgetId }) {
+  function addExpense({ description, amount, budgetId, id }) {
     setExpenses((prevExpenses) => {
-      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+      return [...prevExpenses, { id, description, amount, budgetId }];
     });
   }
 
-  function addBudget({ name, max }) {
+  function addBudget({ name, max, id }) {
     setBudgets((prevBudgets) => {
       if (prevBudgets.find((budget) => budget.name === name)) {
         return prevBudgets;
       }
-      return [...prevBudgets, { id: uuidV4(), name, max }];
+      return [...prevBudgets, { id, name, max }];
     });
   }
 
