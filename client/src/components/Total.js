1afe33e75edcf6a5616e2b78d8e45d02789d5877
dudekrystian <dudekrystian@gmail.com>
2022-12-29 10:16:47 +0100

    rebuilding the component, creating functions that filter the expenses of a specific user

--- client/src/components/Total.js
+++ client/src/components/Total.js
@@ -1,11 +1,36 @@
 import React from "react";
-import useBudgets from "../contexts/budgetsContext";
+import useBudgets from "../context/budgetsContext";
 
 export default function Total() {
-  const { expenses, budgets } = useBudgets();
+  const { budgets, getBudgetExpenses } = useBudgets();
 
-  const totalAmount = expenses.reduce(
-    (total, expense) => total + expense.amount,
+  const currentBudgetsId = budgets.map((budget) => budget._id);
+
+  let arrayExpense = [];
+  const handleValueExpense = currentBudgetsId.forEach((element) =>
+    arrayExpense.push(getBudgetExpenses(element))
+  );
+
+  const totalAmount = arrayExpense.map((element) =>
+    element.map((el) => el.amount)
+  );
+
+  const total = totalAmount.join();
+
+  const totalArrayNumber = total.split(",").map(function (item) {
+    return parseFloat(item, 10);
+  });
+
+  const totalArrayNumber1 = totalArrayNumber.map((item) => {
+    if (isNaN(item)) {
+      return 0;
+    } else {
+      return item;
+    }
+  });
+
+  const totalExpensesAmount = totalArrayNumber1.reduce(
+    (total, expense) => total + expense,
     0
   );
 
@@ -17,7 +42,7 @@ export default function Total() {
   return (
     <>
       <label>
-        Total: {totalAmount}/{totalBudgetsAmount} PLN
+        Total: {totalExpensesAmount}/{totalBudgetsAmount} PLN
       </label>
     </>
   );
