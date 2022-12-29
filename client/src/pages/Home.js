70e7ac0e7fb64943343244bff55caa338c9ac1fb
dudekrystian <dudekrystian@gmail.com>
2022-12-29 10:22:56 +0100

    data transfer from app to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,44 @@
-import React from "react";
+import "../scss/style.css";
+import { useAuthContext } from "../hooks/useAuthContext";
 
-export default function Home() {
-  return <div>Home</div>;
+//components
+import Header from "../components/Header";
+import Panel from "../components/Panel-Expense";
+import BudgetCard from "../components/BudgetCard";
+import useBudgets from "../context/budgetsContext";
+
+function Home() {
+  const { budgets, getBudgetExpenses } = useBudgets();
+
+  const { user } = useAuthContext();
+
+  return (
+    <>
+      <div className="app">
+        <Header user={user} />
+        <Panel />
+
+        <div className="container-budget-card">
+          {budgets.map((budget) => {
+            const amount = getBudgetExpenses(budget._id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            );
+
+            return (
+              <BudgetCard
+                key={budget._id}
+                name={budget.name}
+                amount={amount}
+                max={budget.max}
+                id={budget._id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Home;
