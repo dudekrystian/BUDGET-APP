2c79f7febb86a294cfbe47259c8f51b5a78c4812
dudekrystian <dudekrystian@gmail.com>
2022-12-29 10:18:06 +0100

    add state disabled button and add method which delete expense from db

--- client/src/components/ViewExpensesModal.js
+++ client/src/components/ViewExpensesModal.js
@@ -1,34 +1,52 @@
 import { Modal, Button, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils/utils";
-import useBudgets from "../contexts/budgetsContext";
+import useBudgets from "../context/budgetsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useState } from "react";
 
-export default function ViewExpensesModal({ budgetId, handleClose, show }) {
-  const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
-    useBudgets();
+export default function ViewExpensesModal({
+  budgetId,
+  handleClose,
+  show,
+  name,
+}) {
+  const [disabled, setDisabled] = useState(false);
+  const { getBudgetExpenses } = useBudgets();
+  const { user } = useAuthContext();
 
   const expenses = getBudgetExpenses(budgetId);
-  console.log(expenses);
 
-  const budget = budgets.find((budget) => budget.id === budgetId);
+  const handleClick = async (expense) => {
+    setDisabled(true);
+    if (!user) {
+      return;
+    }
+    const response = await fetch("/api/expenses/" + expense._id, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      // deleteExpense({ json });
+      setDisabled(false);
+    }
+  };
+
+  const red = {
+    color: "red",
+  };
 
   return (
     <Modal size="xl" show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>
           <Stack direction="horizontal" gap="2">
-            <div>Expenses - {budget?.name}</div>
-            {
-              <Button
-                onClick={() => {
-                  expenses.forEach((expenseId) => deleteExpense(expenseId));
-                  deleteBudget(budget);
-                  handleClose();
-                }}
-                variant="outline-danger"
-              >
-                Delete
-              </Button>
-            }
+            <div>
+              Expenses: <strong style={red}> {name} </strong>
+            </div>
           </Stack>
         </Modal.Title>
       </Modal.Header>
@@ -41,8 +59,9 @@ export default function ViewExpensesModal({ budgetId, handleClose, show }) {
                 {currencyFormatter.format(expense.amount)}
               </div>
               <Button
-                onClick={() => deleteExpense(expense)}
-                size="sm"
+                disabled={disabled}
+                onClick={() => handleClick(expense)}
+                size="xl"
                 variant="outline-danger"
               >
                 &times;
