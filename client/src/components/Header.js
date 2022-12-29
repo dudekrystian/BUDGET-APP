f9fd14d57c1ed3b4b58d384d2d31c2f1087e2bbc
dudekrystian <dudekrystian@gmail.com>
2022-12-29 10:15:13 +0100

    add function logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Header() {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <div className="header">
       <span>Budget App</span>
-      <button>Log out</button>
+      <p>
+        Hi <strong>{user.email}</strong>!
+      </p>
+      <button onClick={handleClick}>Log out</button>
     </div>
   );
 }
