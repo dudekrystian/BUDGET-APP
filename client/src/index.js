c8bcf0a8d942227468d228e30073999b6fb7083b
dudekrystian <dudekrystian@gmail.com>
2022-12-29 10:30:12 +0100

    adding a component responsible for authorizations to the application

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BudgetsProvider } from "./contexts/budgetsContext";
+import { BudgetsProvider } from "./context/budgetsContext";
+
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BudgetsProvider>
-      <App />
-    </BudgetsProvider>
+    <AuthContextProvider>
+      <BudgetsProvider>
+        <App />
+      </BudgetsProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
